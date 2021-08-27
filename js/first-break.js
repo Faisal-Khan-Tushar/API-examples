eeb21a45fc4bf65498f054d69a1f6eb1c195057a
Faisal-Khan-Tushar <faisal15-13400@diu.edu.bd>
2021-08-27 19:15:42 +0600

    final one

--- js/first-break.js
+++ js/first-break.js
@@ -1,35 +1,35 @@
-//kanye.rest
-const loadQuote = () => {
-  fetch('https://api.kanye.rest/')
-    .then(res => res.json())
-    .then(data => displayQuote(data))
-}
+// //kanye.rest
+// const loadQuote = () => {
+//   fetch('https://api.kanye.rest/')
+//     .then(res => res.json())
+//     .then(data => displayQuote(data))
+// }
 
-const displayQuote = data => {
-  const blockquote = document.getElementById('quote');
-  // const p = document.createElement('p')
-  // p.innerText = data.quote;
-  blockquote.innerText = data.quote;
+// const displayQuote = data => {
+//   const blockquote = document.getElementById('quote');
+//   // const p = document.createElement('p')
+//   // p.innerText = data.quote;
+//   blockquote.innerText = data.quote;
 
-}
+// }
 //load users
-const loadUsers = () => {
-  fetch('https://randomuser.me/api/?results=5')
-    .then(res => res.json())
-    .then(data => displayUsers(data))
-}
-loadUsers()
-const displayUsers = data => {
-  // console.log(data)
-  const div = document.getElementById('users')
-  const users = data.results;
-  for (const user of users) {
-    const p = document.createElement('p');
-    p.innerText = `Name- ${user.name.title} ${user.name.first} ${user.name.last}. Email-${user.email}`;
-    div.appendChild(p);
+// const loadUsers = () => {
+//   fetch('https://randomuser.me/api/?results=5')
+//     .then(res => res.json())
+//     .then(data => displayUsers(data))
+// }
+// loadUsers()
+// const displayUsers = data => {
+//   // console.log(data)
+//   const div = document.getElementById('users')
+//   const users = data.results;
+//   for (const user of users) {
+//     const p = document.createElement('p');
+//     p.innerText = `Name- ${user.name.title} ${user.name.first} ${user.name.last}. Email-${user.email}`;
+//     div.appendChild(p);
 
-  }
-}
+//   }
+// }
 //load countries
 const loadCountries = () => {
   fetch('https://restcountries.eu/rest/v2/all')
@@ -44,18 +44,26 @@ const displayCountries = countries => {
   //   console.log(country)
   // }
   countries.forEach(country => {
+    // console.log(country);
     const div = document.createElement('div')
     div.classList.add('countries')
-    console.log(div)
-    const h1 = document.createElement('h1');
-    h1.innerText = country.name;
-    div.appendChild(h1)
-    const h2 = document.createElement('h2');
-    h2.innerText = country.capital;
-    div.appendChild(h2);
-    const p = document.createElement('p')
-    p.innerText = country.demonym;
-    div.appendChild(p)
+    div.innerHTML = `
+    <h1>${country.name}</h1>
+    <h2>${country.capital}</h2>
+    <p>${country.demonym}</p>
+    <button onclick="loadDetails(${country})">details</button>
+    `
+
     countriesDiv.appendChild(div);
   })
+}
+const loadDetails = country => {
+  const countryDetails = document.getElementById('countries');
+  const div = document.createElement('div');
+  div.innerHTML = `
+  <h1>${country.name}</h1>
+  <img src="${country.flag}>
+  `;
+  countryDetails.appendChild(div);
+
 }
